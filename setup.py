4a1a199b89726e8be20efb660f402cdbfcd315bb
Harrtron <harleyjthorne@gmail.com>
2019-04-26 15:12:04 +0100

    restructe for deployment, not complete

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='buzzsproutpodcasts',
+      version='0.1',
+      description='Buzzsprout Podcasts Python class',
+      url='https://github.com/Harrtron/buzzsprout-python',
+      author='Harley Thorne',
+      author_email='harleyjthorne@gmail.com',
+      license='MIT',
+      packages=['buzzsprout'],
+      zip_safe=False)
